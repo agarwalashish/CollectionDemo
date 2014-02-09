2ae0353269dd81bda04ddf33e75aa79f0997169c
agarwalashish <ashish-agarwal@hotmail.com>
2014-02-08 17:16:40 -0800

    Initial Commit

---
+++ CollectionDemo/CollectionDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionDemoViewController.h
+//  CollectionDemo
+//
+//  Created by Ashish Agarwal on 2014-02-08.
+//  Copyright (c) 2014 Ashish Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionDemoViewController : UIViewController
+
+@end
---
+++ CollectionDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionDemo
+//
+//  Created by Ashish Agarwal on 2014-02-08.
+//  Copyright (c) 2014 Ashish Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CollectionDemoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CollectionDemoAppDelegate class]));
+    }
+}
